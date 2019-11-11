4688df177da70346bf1e665fbd1d70a598ca3b67
Hyunwoong Chang <hwchang1201@tamu.edu>
2019-11-10 19:33:53 -0600

    implement function "performanceScores".

--- R/performanceScores.R
+++ R/performanceScores.R
@@ -1,9 +1,23 @@
 # performanceScores: ranking and Mean average precision.
 performanceScores <- function(distanceMtx, POS) {
+  N <- ncol(POS)
+  rankingSum <- 0
+  Av.Precision <- 0
+  for (i in 1:N) {
+    # sum of ranking
+    distanceMtx_i <- distanceMtx[POS[, i][2], ]
+    distanceMtx_i[matrix(POS[matrix(rep(POS[1, ] == POS[, i][1], 2), nrow = 2 , byrow = TRUE)], nrow = 2)[2,]] = Inf
+    rankingSum <- rankingSum + which(order(distanceMtx_i) == POS[, i][1])
 
+    # Average precision
+    trueVec <- rep(0, ncol(distanceMtx))
+    trueVec[POS[, i][1]] <- 1
+    Av.Precision <- Av.Precision + averagePrecision(length(trueVec), trueVec, -distanceMtx_i)
+  }
+  ranking <- rankingSum/N
+  MAP <- Av.Precision/N
   return(list(ranking=ranking, MAP=MAP))
 }
-
 # Average Precision.
 averagePrecision <- function (uNumToExamineK, Yreal, Yhat){
 
