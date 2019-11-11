e138d575501ba35bf01b5e6c445df4001a2b2163
Hyunwoong Chang <hwchang1201@tamu.edu>
2019-11-10 19:37:53 -0600

    implement function "embeddingTrainer".

--- R/embeddingTrainer.R
+++ R/embeddingTrainer.R
@@ -59,6 +59,57 @@ getDistanceGradVec <- function(theta_i, theta_j) {
 # embedding trainer -> output : trained theta.
 embeddingTrainer <- function(POS, NEG, entity, theta_dim=2, N_epoch=100, lr=0.2, n_neg=4){
 
+  # Initializing theta
+  theta <- matrix(runif(theta_dim * length(entity), min = -0.001, max = 0.001), ncol = theta_dim)
+
+  for (epoch in 1:N_epoch) {
+    # stochastic gradient method
+    if (epoch == N_epoch %/% 10){
+      lr = 10 * lr
+    }
+    parent_node_length <- length(unique(POS[1, ]))
+    for (k in 1:parent_node_length) {
+      Pos_now <- matrix(POS[matrix(rep(POS[1, ] == k, 2), nrow = 2 , byrow = TRUE)], nrow = 2)
+      j <- sample(1:ncol(Pos_now), 1)
+      pos <- Pos_now[, j]
+      # # pick one vector from positive relation matrix
+      # #j <- sample(1:ncol(D), 1)
+      # pos <- D[, k]
+      # distance between theta[pos[1]], theta[pos[2]]
+      dist_p <- getPoincareDistanceVec(theta[pos[1], ], theta[pos[2]])
+      # derivative of loss function w.r.t distance between theta[pos[1]], theta[pos[2]]
+      der_p <- -1
+      # empty negative relation sampling matrix
+      neg_mat <- matrix(rep(0, 2 * n_neg), ncol = n_neg)
+      # empty vector to store distance between theta[pos[1]], theta[neg[j]] j = 1, 2, .., n_neg
+      dist_neg <- rep(0, n_neg)
+      # making neg_mat, dist_neg
+      for (i in 1:n_neg) {
+        # negative relation matrix subsetting by NEG[1, ] == pos[1]
+        Neg_now <- matrix(NEG[matrix(rep(NEG[1, ] == pos[1], 2), nrow = 2 , byrow = TRUE)], nrow = 2)
+        j <- sample(1:ncol(Neg_now), 1)
+        neg_mat[, i] <- Neg_now[, j]
+        dist_neg[i] <- getPoincareDistanceVec(theta[neg_mat[, i][1], ], theta[neg_mat[, i][2], ])
+      }
+      # denominator of loss function.
+      denom <- sum(exp(-dist_neg))
+      # making der_neg : derivative of loss function w.r.t distance between theta[pos[1]], theta[neg[j]], j = 1, 2, ..., n_neg.
+      der_neg <- rep(0, n_neg)
+      for (i in 1:n_neg) {
+        der_neg[i] <- exp(-dist_neg[i])/denom
+      }
+
+      # update theta[pos[1], ], theta[pos[2], ].
+      theta[pos[1], ] <- proj(theta[pos[1], ] + lr  * (1 - as.numeric(crossprod(theta[pos[1], ])))^2 / 4 * der_p * getDistanceGradVec(theta[pos[1], ], theta[pos[2], ]))
+      theta[pos[2], ] <- proj(theta[pos[2], ] + lr  * (1 - as.numeric(crossprod(theta[pos[2], ])))^2 / 4 * der_p * getDistanceGradVec(theta[pos[2], ], theta[pos[1], ]))
+
+      # update theta[pos[1], ], theta[neg[j], ], j = 1, 2, ..., n_neg.
+      for (i in 1:n_neg) {
+        theta[neg_mat[, i][1], ] <- proj(theta[neg_mat[, i][1], ] + lr * (1 - as.numeric(crossprod(theta[neg_mat[, i][1], ])))^2 / 4 * der_neg[i] * getDistanceGradVec(theta[neg_mat[, i][1], ], theta[neg_mat[, i][2], ]))
+        theta[neg_mat[, i][2], ] <- proj(theta[neg_mat[, i][2], ] + lr * (1 - as.numeric(crossprod(theta[neg_mat[, i][2], ])))^2 / 4 * der_neg[i] * getDistanceGradVec(theta[neg_mat[, i][2], ], theta[neg_mat[, i][1], ]))
+      }
+    }
+  }
 
   return(theta)
 }
