9c744dae8fa999c0af3edcde8e951b7284d5e340
Hyunwoong Chang <hwchang1201@tamu.edu>
2019-11-12 17:08:47 -0600

    change the input type of function to tree-shape dataset from "data.tree"

--- R/dataLoader.R
+++ R/dataLoader.R
@@ -1,13 +1,27 @@
-library("yaml")
-library("data.tree")
+# library("yaml")
+# library("data.tree")
 
-dataLoader_yaml <- function(yamlDataset){
-  # loading yaml dataset.
-  dataset <- yaml.load(yamlDataset)
-  # convert it with tree structure.
-  dataset_tree <- as.Node(dataset)
+#' A function that convert a tree-shape dataset to the components for poincare-embeddings.
+#'
+#' @param dataset_tree - A tree-shape dataset with yaml format.
+#'
+#' @return entity - A vector of all the entities in the tree-shape dataset.
+#' @return POS - A 2-row positive relation matrix that contains positive relation entities columnwisely.
+#' @return NEG - A 2-row negative relation matrix that contains negative relation entities columnwisely.
+#' @export
+#'
+#' @examples statistics_yaml <- yaml::yaml.load(statistics)
+#' @examples statistics_tree <- data.tree::as.Node(statistics_yaml)
+#' @examples dataLoader(statistics_tree)
+
+
+dataLoader <- function(dataset_tree){
+  # check the compatibility.
+  if (typeof(dataset_tree) != "environment"){
+    stop("Please make sure your input is tree structure dataset from data.tree")
+  }
   # name of all nodes in dataset.
-  entity <- unique(c(as.vector(as.matrix(ToDataFrameNetwork(dataset_tree)["from"])), as.vector(as.matrix(ToDataFrameNetwork(dataset_tree)["to"]))))
+  entity <- unique(c(as.vector(as.matrix(data.tree::ToDataFrameNetwork(dataset_tree)["from"])), as.vector(as.matrix(data.tree::ToDataFrameNetwork(dataset_tree)["to"]))))
   # proxy : list that numerate on the name of nodes.
   proxy <- vector(mode = "list", length = length(entity))
   names(proxy) <- entity
@@ -15,25 +29,26 @@ dataLoader_yaml <- function(yamlDataset){
     proxy[i] <- i
   }
   # positive relation matrix
-  POS <- matrix(rep(0, 2 * length(ToDataFrameNetwork(dataset_tree)[,1])), nrow = 2)
+  POS <- matrix(rep(0, 2 * length(data.tree::ToDataFrameNetwork(dataset_tree)[,1])), nrow = 2)
   for (i in 1:2){
     k = 1
-    for (j in as.vector(as.matrix(ToDataFrameNetwork(dataset_tree)[,i]))){
+    for (j in as.vector(as.matrix(data.tree::ToDataFrameNetwork(dataset_tree)[,i]))){
       POS[i, k] <- proxy[[j]]
       k = k + 1
     }
   }
   NEG = matrix(, nrow = 2, ncol = 0)
+
   # negative relation matrix
-  for (i in as.matrix(unique(ToDataFrameNetwork(dataset_tree)['from']))){
+  for (i in as.matrix(unique(data.tree::ToDataFrameNetwork(dataset_tree)['from']))){
     neg_vec <- 1:length(entity)
-    for (j in as.matrix(ToDataFrameNetwork(dataset_tree)['to'][ToDataFrameNetwork(dataset_tree)['from'] == i])){
+    for (j in as.matrix(data.tree::ToDataFrameNetwork(dataset_tree)['to'][data.tree::ToDataFrameNetwork(dataset_tree)['from'] == i])){
       neg_vec <- neg_vec[!neg_vec == proxy[[j]]]
     }
     parent <- i
     while (length(parent) != 0){
-      if (sum(ToDataFrameNetwork(dataset_tree)['to'] == parent) > 0){
-        parent = ToDataFrameNetwork(dataset_tree)['from'][ToDataFrameNetwork(dataset_tree)['to'] == parent]
+      if (sum(data.tree::ToDataFrameNetwork(dataset_tree)['to'] == parent) > 0){
+        parent = data.tree::ToDataFrameNetwork(dataset_tree)['from'][data.tree::ToDataFrameNetwork(dataset_tree)['to'] == parent]
         neg_vec <- neg_vec[!neg_vec == proxy[[parent]]]
       }else{
         break
@@ -47,4 +62,6 @@ dataLoader_yaml <- function(yamlDataset){
   }
 
   return(list(entity = entity, POS = POS, NEG = NEG))
+
 }
+
