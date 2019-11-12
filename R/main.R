3c5e98f245ed8ead209cad6d7d87023c2510f23d
Hyunwoong Chang <hwchang1201@tamu.edu>
2019-11-12 17:14:07 -0600

    add example of "statistics_adv" in "toyDataset_yaml.R".

--- R/main.R
+++ R/main.R
@@ -1,13 +1,30 @@
 # main function defined inputs and outputs
-source("embeddingTrainer.R")
-source("illustration.R")
-source("performanceScores.R")
-source("ToyDataset_yaml.R")
-source("dataLoader.R")
+# source("embeddingTrainer.R")
+# source("illustration.R")
+# source("performanceScores.R")
+# source("ToyDataset_yaml.R")
+# source("dataLoader.R")
 
-poincareEmbeddings <- function(yamlDataset, theta_dim=2, N_epoch=50, lr=0.01, n_neg=5){
+#' Poincare-embeddings
+#'
+#' @param dataset_tree - A tree-shape dataset from "data.tree"
+#' @param theta_dim - The dimension of the embedding space.
+#' @param N_epoch - The number of epochs.
+#' @param lr - The learning rate.
+#' @param n_neg - The number of negative samples for each iteration.
+#'
+#' @return Ranking : The ranking of the true related entity.
+#' @return MAP : Mean average precision.
+#' @return A 2-dimension plot only if theta_dim = 2.
+#' @export
+#'
+#' @examples statistics_adv_yaml <- yaml::yaml.load(statistics_adv)
+#' @examples statistics_adv_tree <- data.tree::as.Node(statistics_adv_yaml)
+#' @examples poincareEmbeddings(statistics_adv_tree, theta_dim = 2, N_epoch = 200, lr = 0.005, n_neg = 10)
+#'
+poincareEmbeddings <- function(dataset_tree, theta_dim=2, N_epoch=50, lr=0.01, n_neg=5){
   #retrieve dataset.
-  dataset <- dataLoader_yaml(yamlDataset)
+  dataset <- dataLoader(dataset_tree)
   #make embedding.
   theta <- embeddingTrainer(dataset$POS, dataset$NEG, dataset$entity, theta_dim, N_epoch, lr, n_neg)
   # if dimension of theta is 2, we can plot the embedding.
@@ -17,5 +34,5 @@ poincareEmbeddings <- function(yamlDataset, theta_dim=2, N_epoch=50, lr=0.01, n_
   }
   # from "performanceScores.R"
   performance <- performanceScores(getPoincareDistance(theta), dataset$POS)
-  return(list(rank = performance$ranking, map = performance$MAP))
+  return(list(theta = theta, rank = performance$ranking, map = performance$MAP))
 }
